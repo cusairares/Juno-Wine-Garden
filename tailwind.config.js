06a6a2ea7c9dc60db0831074a77df32c6e929f6c
Pavel Constantin-Beniamin <constantin.pavel@student.unitbv.ro>
2025-11-09 18:27:20 +0200

    color preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors:{
-        primary:"#030014"
+      colors: {
+        'primary': '#030014',
+        'secundary': '#151312',
+        'light': {
+          100: '#D6C7FF',
+          200: '#A8DB5DB',
+          300: '#9CA4AB'
+        },
+        'dark': {
+          100: '#221f3d',
+          200: '#0f0d23',
+        },
+        'accent':'#AB8BFF'
       }
     },
   },
