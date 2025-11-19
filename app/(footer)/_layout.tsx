c78dc3212d36d27666e7da1513f9f3f1f94fbde0
DantonB <burceadavid0@gmail.com>
2025-11-19 22:58:11 +0200

    First commit/Tabs and their Icons

--- app/(footer)/_layout.tsx
+++ app/(footer)/_layout.tsx
@@ -1,7 +1,22 @@
 import { icons } from "@/constants/icons";
 import { Tabs } from "expo-router";
-import React from "react";
-import { Image, Pressable, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
+
+
+const TabWithIcon = ({ label, icon, focused }: { label:string; icon: any; focused: boolean }) => {
+    const color = focused ? "#50827D" : "#7F7F7F"
+    return (
+        <View style={styles.tabContainer}>
+            <Image
+                source={icon}
+                style={{ width: 18, height: 18, tintColor: color }}
+                resizeMode="contain"
+            />
+        </View>
+    )
+}
+
+
 
 
 const _Layout = () =>{
@@ -20,12 +35,50 @@ const _Layout = () =>{
                             )}
                         </Pressable>
                     </View>
-                )
+                ),
+                tabBarActiveTintColor: "#50827D", // color for selected label + icon
+            tabBarInactiveTintColor: "#999",  // color for unselected label + icon
+            tabBarLabelStyle: { fontSize: 12 }, // optional, adjust label size
             }}>
-            <Tabs.Screen name="index" options={{ title: "Home" }} />
-            <Tabs.Screen name="meniu" options={{ title: "Meniu" }} />
+            <Tabs.Screen name="index"
+            options={{
+                 title: "Home" ,
+                 tabBarIcon: ({ focused }) =>
+                 <TabWithIcon label="Home" icon={icons.home} focused={focused}/>,
+                }}
+            />
+
+
+            <Tabs.Screen name="menu" 
+            options={{
+                 title: "Menu",
+                 tabBarIcon: ({ focused }) =>
+                 <TabWithIcon label="Menu" icon={icons.menu} focused={focused}/>,
+                 }} 
+            />
+            <Tabs.Screen name="orders" 
+            options={{ 
+                title: "Orders",
+                tabBarIcon: ({ focused }) =>
+                <TabWithIcon label="Orders" icon={icons.orders} focused={focused}/>,
+            }} 
+            />
+            <Tabs.Screen name="cart" 
+            options={{ 
+                title: "Cart",
+                tabBarIcon: ({ focused }) =>
+                <TabWithIcon label="Cart" icon={icons.cart} focused={focused}/>,
+            }} 
+            />
         </Tabs>
     )
 }
+
+const styles = StyleSheet.create({
+    tabContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    }
+})
 
 export default _Layout
