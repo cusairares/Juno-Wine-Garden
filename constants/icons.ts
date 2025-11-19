3138bd343ec0e87aff18f258337107df887e38b8
DantonB <burceadavid0@gmail.com>
2025-11-19 22:58:11 +0200

    Added wine/food/bar Data + Menu logic
    added footer

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,7 +1,9 @@
 import arrow from "@/assets/icons/arrow.png";
-import cart from "@/assets/icons/cart.png";
-import home from "@/assets/icons/home.png";
+import cart from "@/assets/icons/cart1_g.png";
+import home from "@/assets/icons/home1_g.png";
 import logo from "@/assets/icons/logo.png";
+import menu from "@/assets/icons/menu_g.png";
+import orders from "@/assets/icons/orders_g.png";
 import person from "@/assets/icons/person.png";
 import play from "@/assets/icons/play.png";
 import save from "@/assets/icons/save.png";
@@ -10,6 +12,8 @@ import star from "@/assets/icons/star.png";
 
 export const icons = {
   home,
+  orders,
+  menu,
   search,
   person,
   logo,
